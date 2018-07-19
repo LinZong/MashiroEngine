9db7b1d2de4fa509f46264741ee11d906218229e
NemesissLin <353388639@qq.com>
2018-07-19 17:53:28 +0800

    Add simple css style

--- src/Views/TextBox.js
+++ src/Views/TextBox.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import './GameView.css'
-const TextBox = ({ SectionName,CharacterName, TextContent, MouseEventTrigger }) => (
+import './TextBoxView.css'
+const TextBox = ({ SectionName, CharacterName, TextContent, MouseEventTrigger }) => (
     <div className="App">
-        <Link to='/'>返回到章节选择</Link>
+        <p className="App-title" id="SectionName">{SectionName}</p>
+        <Link to='/' className="App-title">返回到章节选择</Link>
         <div className="TextBox" onMouseDown={() => MouseEventTrigger({ Mouse: true })}>
-            <p className="App-title" id="SectionName">{SectionName}</p>
             <p className="App-intro" id="CharacterName">{CharacterName}</p>
             <p className="App-intro" id="Text">{TextContent}</p>
         </div>
