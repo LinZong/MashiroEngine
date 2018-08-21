fdb87b22c12a9c161833781500a710570463f5c0
LinZong <353388639@qq.com>
2018-08-21 15:29:03 +0800

    添加了旁白PlainText可自动向下滚动

--- src/Views/PlainText/PlainText.js
+++ src/Views/PlainText/PlainText.js
@@ -9,13 +9,16 @@ class PlainText extends React.Component {
 			TextArray: [this.props.CurrentText]
 		}
 		this.StopTyping = this.StopTyping.bind(this);
+		this.ScrollToEnd=this.ScrollToEnd.bind(this);
 		this.TypeSpeed = window.electron.remote.getGlobal('SettingsNode')['TEXT_SETTING']['SettingElement']['LeftCol'][0].Value;
-
+		this.ScrollIntervalHandle = null;
 	}
-	//组件内部维护一个文本数组，按顺序记录所有被放进了CurrentText 的Prop的文本，就好像Stack一样，控制最后的一段文本有打字机效果，其他的文本是普通的<p>
+	ScrollToEnd(){
+		this.DocumentRoot.scrollTop=this.DocumentRoot.scrollHeight;
+	}
 	componentDidMount() {
-		//TextArray.push(this.props.CurrentText);
 		this.props.GetStopTyping(this.StopTyping);
+		this.DocumentRoot = document.getElementsByClassName('PlainText')[0];
 	}
 	componentWillReceiveProps(nextProps, nextState) {
 		if (nextProps.CurrentText !== this.props.CurrentText) {
@@ -32,36 +35,52 @@ class PlainText extends React.Component {
 	}
 	StopTyping() {
 		this.typed.destroy();
+		this.ScrollToEnd();
+		clearInterval(this.ScrollIntervalHandle);
 		document.getElementById("Text").innerText = this.props.CurrentText;
 	}
 	componentWillUnmount() {
 		if (this.hasOwnProperty('typed') || this.typed !== undefined) {
 			this.typed.destroy();
 		}
+		clearInterval(this.ScrollIntervalHandle);
 	}
 	render() {
 		return (
 			<ControlFunctionContext.Consumer>
+
 				{Func =>
-					<div className="PlainText" onClick={() => this.props.MouseEventTrigger({ Mouse: true })}>
-						{
-							this.state.TextArray.map((it, idx) => {
-								return idx < this.state.TextArray.length - 1 || this.props.Rollback ? (<p key={idx}>{it}</p>) : (
-									<Typed
-										typedRef={(typed) => { this.typed = typed; }}
-										key={idx}
-										strings={[it]}
-										typeSpeed={this.TypeSpeed}
-										showCursor={false}
-										preStringTyped={() => Func.SetTypingStatus(1)}
-										onComplete={() => Func.SetTypingStatus(0)}
-										onDestroy={() => Func.SetTypingStatus(0)}>
-										<p id="Text" />
-									</Typed>
-								)
-							}, this)
-						}
+					<div id="ScrollWrapper">
+						<div className="PlainText" onClick={() => this.props.MouseEventTrigger({ Mouse: true })}>
+							{
+								this.state.TextArray.map((it, idx) => {
+									return idx < this.state.TextArray.length - 1 || this.props.Rollback ? (<p key={idx}>{it}</p>) : (
+										<Typed
+											typedRef={(typed) => { this.typed = typed; }}
+											key={idx}
+											strings={[it]}
+											typeSpeed={this.TypeSpeed}
+											showCursor={false}
+											preStringTyped={() => {
+												Func.SetTypingStatus(1)
+												this.ScrollIntervalHandle=setInterval(()=>{
+													this.ScrollToEnd();
+												},50);
+											}}
+											onComplete={() => {
+												this.ScrollToEnd();
+												Func.SetTypingStatus(0)
+												clearInterval(this.ScrollIntervalHandle);
+											}}
+											onDestroy={() => Func.SetTypingStatus(0)}>
+											<p id="Text" />
+										</Typed>
+									)
+								}, this)
+							}
+						</div>
 					</div>}
+
 			</ControlFunctionContext.Consumer>
 		);
 	}
